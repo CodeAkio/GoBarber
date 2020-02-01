6bc4cfca54063f32508e6f8b7c472ed2edce40e6
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-02-01 00:13:20 -0300

    Cancel Appointment and Send E-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b769c8ed5a8cfb',
+    pass: 'abb3ef9fb948e6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobaerber.com>',
+  },
+};
