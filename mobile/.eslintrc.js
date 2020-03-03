5e963907ad9f9fa4ff3b76bd8bda40441d44c92c
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-03 13:05:12 -0300

    Mobile: React navigation

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -51,8 +51,8 @@ module.exports = {
   settings: {
     'import/resolver': {
       'babel-plugin-root-import': {
-        rootPathSuffix: 'src'
-      }
-    }
-  }
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
