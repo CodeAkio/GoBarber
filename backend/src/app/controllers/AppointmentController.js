31d6a7bb4dee47e28a8ab453b92670455789f62e
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-19 17:27:19 -0300

    Backend: Fix async response of cancel appointment service in controller

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -47,7 +47,7 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = CancelAppointmentService.run({
+    const appointment = await CancelAppointmentService.run({
       provider_id: req.params.id,
       user_id: req.userId,
     });
